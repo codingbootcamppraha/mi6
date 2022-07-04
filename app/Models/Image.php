23df663679ac33a78d17769cf5afb285191c93e8
Ondřej Hotový <ondrej.hotovy@outlook.com>
2022-07-04 11:36:46 +0200

    Rights fun

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Person;
+
 class Image extends Model
 {
     use HasFactory;
+
+    public function person()
+    {
+        return $this->hasOne(Person::class);
+    }
 }
